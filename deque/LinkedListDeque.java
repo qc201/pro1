b7c850e2ad6f6619aedcb928a6e0a9280d1c3933
Qiuyu <qchenla@gmail.com>
2023-01-27 16:05:02 -0800

    deque completed

---
+++ deque/LinkedListDeque.java
@@ -0,0 +1,139 @@
+package deque;
+
+public class LinkedListDeque<MyDataType> {
+    private MyNode dummy;
+    private int size;
+
+    // a single node item in the linked list
+    public class MyNode {
+        public MyDataType val;
+        public MyNode prev;
+        public MyNode next;
+        public MyNode(MyDataType x, MyNode prevNode, MyNode nextNode) {
+            val = x;
+            prev = prevNode;
+            next = nextNode;
+        }
+    }
+    // construct a new empty linked list with dummy head
+    // dummy.prev = tail
+    // dummy.next = head
+    public LinkedListDeque(MyDataType x) {
+        dummy = new MyNode(x, null, null);
+        MyNode newNode = new MyNode(x, dummy, dummy);
+        dummy.next = newNode;
+        dummy.prev = newNode;
+        size = 1;
+    }
+
+    public LinkedListDeque() {
+        dummy = new MyNode(null, null, null);
+        dummy.prev = dummy;
+        dummy.next = dummy;
+        size = 0;
+    }
+
+    public LinkedListDeque(LinkedListDeque other) {
+        dummy = new MyNode(null, null, null);
+        dummy.prev = dummy;
+        dummy.next = dummy;
+        size = 0;
+        for (int i=0; i < other.size; i++) {
+            addLast((MyDataType) other.get(i));
+        }
+    }
+
+
+    public void addLast(MyDataType n) {
+        MyNode prevNode = dummy.prev;
+        MyNode newNode = new MyNode(n, prevNode, dummy);
+        prevNode.next = newNode;
+        dummy.prev = newNode;
+        size += 1;
+    }
+
+    public void addFirst(MyDataType n) {
+        MyNode nextNode = dummy.next;
+        MyNode newNode = new MyNode(n, dummy, nextNode);
+        dummy.next = newNode;
+        nextNode.prev = newNode;
+        size += 1;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public MyDataType removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        MyNode curNode = dummy.next;
+        dummy.next = curNode.next;
+        curNode.next.prev = dummy;
+        size -= 1;
+        return curNode.val;
+    }
+
+    public MyDataType removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        MyNode lastNode = dummy.prev;
+        lastNode.prev.next = dummy;
+        dummy.prev = lastNode.prev;
+        size -= 1;
+        return lastNode.val;
+    }
+
+    public MyDataType get(int index) {
+        MyNode pointer = dummy.next;
+        int counter = 0;
+        while (pointer != dummy && counter < index) {
+            pointer = pointer.next;
+            counter += 1;
+        }
+        if (counter < index) {
+            return null;
+        } else {
+            return pointer.val;
+        }
+    }
+
+    public MyDataType getRecursive(int index) {
+        return get(index);
+    }
+
+    public void printDeque() {
+        MyNode pointer = dummy.next;
+        while (pointer != dummy) {
+            System.out.print(pointer.val + " ");
+            pointer = pointer.next;
+        }
+        System.out.println(" ");
+    }
+
+
+
+
+//    public static void main(String[] args) {
+//        LinkedListDeque<Integer> myNum = new LinkedListDeque<Integer>();
+//        myNum.addLast(0);
+//        myNum.addLast(1);
+//        myNum.addLast(2);
+//        myNum.addLast(3);
+//        myNum.addFirst(-1);
+//        myNum.removeFirst();
+//        myNum.printDeque();
+//        LinkedListDeque newNums = new LinkedListDeque(myNum);
+//        newNums.removeFirst();
+//        newNums.printDeque();
+//        System.out.println("original list:");
+//        myNum.printDeque();
+//    }
+
+}
